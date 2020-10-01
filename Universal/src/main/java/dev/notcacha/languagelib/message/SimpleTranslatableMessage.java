bdbc2d21c1bd7cafde9dadb9374cfbbd37583790
cassha <notcacha1998@gmail.com>
2020-10-01 05:05:03 +0200

    Update MessageColorApplier in SimpleTranslatableMessage

--- Universal/src/main/java/dev/notcacha/languagelib/message/SimpleTranslatableMessage.java
+++ Universal/src/main/java/dev/notcacha/languagelib/message/SimpleTranslatableMessage.java
@@ -106,22 +106,24 @@ public class SimpleTranslatableMessage implements TranslatableMessage {
         return this;
     }
 
-    static class MessageColorApplier {
+}
 
-        private static final char COLOR_CHAR = '\u00A7';
-        private static final String ALL_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
+class MessageColorApplier {
 
-        public static String apply(char altColorChar, String textToTranslate) {
-            char[] b = textToTranslate.toCharArray();
-            for (int i = 0; i < b.length - 1; i++) {
-                if (b[i] == altColorChar && ALL_CODES.indexOf(b[i + 1]) > -1) {
-                    b[i] = COLOR_CHAR;
-                    b[i + 1] = Character.toLowerCase(b[i + 1]);
-                }
+    private static final char COLOR_CHAR = '\u00A7';
+    private static final String ALL_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
+
+    public static String apply(char altColorChar, String textToTranslate) {
+        char[] b = textToTranslate.toCharArray();
+
+        for (int i = 0; i < b.length - 1; i++) {
+            if (b[i] == altColorChar && ALL_CODES.indexOf(b[i + 1]) > -1) {
+                b[i] = COLOR_CHAR;
+                b[i + 1] = Character.toLowerCase(b[i + 1]);
             }
-            return new String(b);
         }
 
+        return new String(b);
     }
 
 }
