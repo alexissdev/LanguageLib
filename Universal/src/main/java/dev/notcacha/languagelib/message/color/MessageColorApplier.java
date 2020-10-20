a71b3204027af37a6f910fadb3d099e9fa37c389
cassha <notcacha1998@gmail.com>
2020-10-20 20:20:31 +0200

    Update MessageColorApplier

--- Universal/src/main/java/dev/notcacha/languagelib/message/color/MessageColorApplier.java
+++ Universal/src/main/java/dev/notcacha/languagelib/message/color/MessageColorApplier.java
@@ -2,15 +2,24 @@ package dev.notcacha.languagelib.message.color;
 
 public interface MessageColorApplier {
 
+    /**
+     * Set color from message
+     *
+     * @param altColorChar    the {@link Character} that will be changed by the {@link Character} that sets the color
+     * @param textToTranslate text to be set color
+     * @return {@param textToTranslate} with set colors
+     */
+
     static String apply(char altColorChar, String textToTranslate) {
-        char COLOR_CHAR = '\u00A7';
-        String ALL_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
+        char color = '\u00A7';
+
+        String codes = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
 
         char[] b = textToTranslate.toCharArray();
 
         for (int i = 0; i < b.length - 1; i++) {
-            if (b[i] == altColorChar && ALL_CODES.indexOf(b[i + 1]) > -1) {
-                b[i] = COLOR_CHAR;
+            if (b[i] == altColorChar && codes.indexOf(b[i + 1]) > -1) {
+                b[i] = color;
                 b[i + 1] = Character.toLowerCase(b[i + 1]);
             }
         }
